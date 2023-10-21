d69be07f05252a51b9b6fe381299c5663bf0d641
Jonathan Pereira <jonathanpereira.jsp@outlook.com>
2023-10-21 09:25:29 -0300

    updating footer, back to top and whatsapp button

--- js/scroll-suave.js
+++ js/scroll-suave.js
@@ -2,27 +2,21 @@
 const linkInterno = document.querySelector('[data-scroll="suave"] a[href^="#"]');
 
 
-function ScrollTop(event) {
-    event.preventDefault()
+export function ScrollTop(event) {
+    event.preventDefault();
 
     const href = event.currentTerget.getAtribute('href');
 
-    const ancora = document.querySelector(href)
+    const ancora = document.querySelector(href);
 
     ancora.scrollIntroView({
         behavior: 'smooth',
         block: 'start',
     })
 
-
-
-
-
-
 }
 
-
-linkInterno.addEventListener('clikck, ScrollTop');
+linkInterno.addEventListener('click, ScrollTop');
 
 
 
