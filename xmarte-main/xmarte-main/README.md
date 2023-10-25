b62f8218f39e03714fc2d430dd8119fe6d2de7c6
Jonathan Pereira <jonathanpereira.jsp@outlook.com>
2023-10-25 20:54:27 -0300

    exclusão duplicada

--- xmarte-main/xmarte-main/README.md
+++
@@ -1,2 +0,0 @@
-# xmarte
-Projeto de Desenvolvimento Web em HTML, CSS, JS e PHP
--- xmarte-main/xmarte-main/script.js
+++
@@ -1,5 +0,0 @@
-import {animarScroll} from './js/scroll-animation.js';
-import {ScrollTop} from './js/scroll-suave.js';
-
-animarScroll();
-ScrollTop();
